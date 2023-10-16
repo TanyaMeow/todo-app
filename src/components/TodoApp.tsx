7dba13fceef080571171dfc03a6ed612d0975eb0
Татьяна Хонякина <hanakinat@gmail.com>
2023-10-16 09:49:25 +0300

    Изменено название заголовка

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -11,7 +11,7 @@ export class TodoApp extends Component<any, any>{
                     <div className="wrapper">
                         <div className="todo_task_container">
                             <div className="header_todo">
-                                <h1 className="title">TODO Task</h1>
+                                <h1 className="title">TODOTask</h1>
                                 <FunctionalTasks />
                             </div>
                             <TasksContainer />
