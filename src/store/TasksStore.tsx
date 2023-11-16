96fc701064a64de52fe7c891d28ba5bee79e4681
Gevorg Danelyan <g.danelyan@worksolutions.ru>
2023-11-16 13:21:08 +0300

    Добавлены замечания

--- src/store/TasksStore.tsx
+++ src/store/TasksStore.tsx
@@ -1,29 +1,41 @@
 import {makeAutoObservable} from "mobx";
+// FIXME store не может зависеть он модулей визуализации.
+//  Перенеси TaskInterface сюда
 import {TaskInterface} from "../components/TodoApp";
 import {MockTodoApi} from "../TodoApi/TodoApi";
 
 class TasksStore {
+    // FIXME вынеси в конструктор, передавай инстанс в момент создания стора
     private todoApi: MockTodoApi = new MockTodoApi();
     tasks: TaskInterface[] = []
 
     constructor() {
         makeAutoObservable(this, undefined, {autoBind: true});
+        // FIXME стор не может знать, когда ему следует загрузить данные
+        //  если я зайду в приложение и не будут использовать страницу с туду, то они все равно загрузятся
+        //  это избыточная нагрузка на сервер + избытычные действия для клиента
         this.loadTasks();
     }
 
     private loadTasks(): void {
+        // FIXME мы не используем then, если можем использовать await
+        //  читаемость кода в цепочках сильно ниже
+        // FIXME поправь везде где есть then
         this.todoApi.get()
             .then((tasks: TaskInterface[]): void => {
                 this.tasks = tasks;
             })
     }
 
+    // FIXME нет смысла в этой функции, tasks - public
     public getTasks(): TaskInterface[] {
         return this.tasks;
     }
 
     public createTask(task: TaskInterface): void {
         this.todoApi.post(task)
+            // FIXME метод отвечает за создание таски, он не должен запрашивать данные
+            //  ты можешь запросить данные внутри компонента, либо тут можешь руками добавить новую таску в this.tasks
             .then(() => this.todoApi.get())
             .then((tasks: TaskInterface[]): void => {
                 this.tasks = tasks;
@@ -32,6 +44,7 @@ class TasksStore {
 
     public updateTasks(task: TaskInterface): void {
         this.todoApi.update(task)
+            // FIXME логика такая же как и при создании
             .then(() => this.todoApi.get())
             .then((tasks: TaskInterface[]): void => {
                 this.tasks = tasks
@@ -40,6 +53,7 @@ class TasksStore {
 
     public removeTask(id: number): void {
         this.todoApi.delete(id)
+            // FIXME логика такая же как и при создании
             .then(() => this.todoApi.get())
             .then((tasks: TaskInterface[]): void => {
                 this.tasks = tasks;
@@ -48,6 +62,7 @@ class TasksStore {
 
     public markCompletedTasks(): void {
         this.todoApi.markTasksCompleted()
+            // FIXME логика такая же как и при создании
             .then(() => this.todoApi.get())
             .then((tasks: TaskInterface[]): void => {
                 this.tasks = tasks;
@@ -56,6 +71,7 @@ class TasksStore {
 
     public deleteCompletedTasks(): void {
         this.todoApi.deleteCompletedTasks()
+            // FIXME логика такая же как и при создании
             .then(() => this.todoApi.get())
             .then((tasks: TaskInterface[]): void => {
                 this.tasks = tasks;
